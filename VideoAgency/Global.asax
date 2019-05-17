90231d31e879d4e18aaa9bb5cf5ea27bb3161de0
Tony Trương <mingonhaohao@gmail.com>
2019-05-17 20:45:19 +0700

    Add project files.

---
+++ VideoAgency/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VideoAgency.MvcApplication" Language="C#" %>
---
+++ VideoAgency/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VideoAgency.Startup))]
+namespace VideoAgency
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
